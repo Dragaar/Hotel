abc40764f167167fe9e1725495593bba989a67c8
Dragaar <rosivanyshyn@gmail.com>
2022-11-22 16:56:25 +0200

    Fix displaying servlet
    Servlet didn`t displaying on client side cause newest jakarta library not support Tomcat 7. Rollback to old javax.servlet lib fixed problem

--- src/main/java/com/rosivanyshyn/controller/HelloServlet.java
+++ src/main/java/com/rosivanyshyn/controller/HelloServlet.java
@@ -1,8 +1,8 @@
 package com.rosivanyshyn.controller;
 
 import java.io.*;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
