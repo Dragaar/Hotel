41673c2a4383e181f3fc499c489ccd06df0d8a74
Dragaar <rosivanyshyn@gmail.com>
2022-11-29 18:34:57 +0200

    Add general interface as root of DAO

---
+++ src/main/java/com/rosivanyshyn/db/dao/GenericDAO.java
@@ -0,0 +1,59 @@
+package com.rosivanyshyn.db.dao;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+
+public interface GenericDAO<T> {
+
+    /** Add specific object to respective table.
+     *
+     * @param object object
+     * @param con connection to database
+     * @return Boolean
+     */
+    Boolean         insert(Connection con, T object);
+
+    /** Get object from respective table by id.
+     *
+     * @param id object id
+     * @param con connection to database
+     * @return T object
+     */
+    T               get(Connection con, Long id);
+
+    /** Get all objects from table.
+     *
+     * @param con connection to database
+     * @return ArrayList<T>
+     */
+    ArrayList<T>    getAll(Connection con);
+
+    /** Find entity from respective table by field and value.
+     *
+     * @param field user field
+     * @param value user value
+     * @param con connection to database
+     * @return T
+     */
+    T               getByField(Connection con, String field, Object value);
+
+    /** Update object in respective table.
+     *
+     * @param object object to update
+     * @param con connection to database
+     * @return Boolean
+     */
+    Boolean         update(Connection con, T object);
+
+    /** Delete object from respective table by id.
+     *
+     * @param id object id
+     * @param con connection to database
+     * @return Boolean
+     */
+    Boolean         delete(Connection con, Long id);
+
+
+
+}
