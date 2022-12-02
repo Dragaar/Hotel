71616b65e8a154b577b5edbec3037dd68a056bfe
Dragaar <rosivanyshyn@gmail.com>
2022-12-02 20:39:57 +0200

    Update DAO interface
    Add many-to-many queries

--- src/main/java/com/rosivanyshyn/db/dao/ResponseToOrderDAO.java
+++ src/main/java/com/rosivanyshyn/db/dao/ResponseToOrderDAO.java
@@ -1,6 +1,17 @@
 package com.rosivanyshyn.db.dao;
 
+import com.rosivanyshyn.db.dao.entity.Apartment;
 import com.rosivanyshyn.db.dao.entity.ResponseToOrder;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+
 public interface ResponseToOrderDAO extends GenericDAO<ResponseToOrder> {
+
+    //Many-to-Many relationship
+    boolean setApartmentToResponse(Connection con, ResponseToOrder rto, Apartment apartment);
+    ArrayList<Apartment> getResponseApartments(Connection con, Long id);
+
+    boolean deleteResponseApartments(Connection con, Long id);
+
 }
