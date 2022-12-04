7af272f0265c6b2e564559c5b04c74a01c31a031
Dragaar <rosivanyshyn@gmail.com>
2022-12-04 13:48:47 +0200

    Add exception message constants

---
+++ src/main/java/com/rosivanyshyn/exeption/Message.java
@@ -0,0 +1,12 @@
+package com.rosivanyshyn.exeption;
+
+public class Message {
+    public static final String INSERT_ERROR = "Cannot create row";
+    public static final String SELECT_ERROR = "Cannot find row";
+    public static final String SELECT_ALL_ERROR = "Cannot find all rows";
+    public static final String SELECT_BY_FIELD_ERROR = "Cannot find row by given field and value";
+    public static final String UPDATE_ERROR = "Cannot update row by given object";
+    public static final String DELETE_ERROR = "Cannot delete row by given id";
+    public static final String MANY_TO_MANY_ERROR = "Cannot operate with ManyToMany";
+
+}
