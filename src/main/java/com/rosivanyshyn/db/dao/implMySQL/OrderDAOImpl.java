2d03b7e10200e8b0f7d3d64f1e527361c3335c4c
Dragaar <rosivanyshyn@gmail.com>
2022-12-09 19:45:40 +0200

    Add Order DAO impl and test

---
+++ src/main/java/com/rosivanyshyn/db/dao/implMySQL/OrderDAOImpl.java
@@ -0,0 +1,113 @@
+package com.rosivanyshyn.db.dao.implMySQL;
+
+import com.mysql.cj.xdevapi.Type;
+import com.rosivanyshyn.db.dao.OrderDAO;
+import com.rosivanyshyn.db.dao.entity.*;
+import com.rosivanyshyn.db.manager.DBManager;
+
+import java.sql.Connection;
+import java.sql.Types;
+
+import static com.rosivanyshyn.db.dao.constant.Field.*;
+import static com.rosivanyshyn.db.dao.constant.Query.*;
+
+public class OrderDAOImpl extends GenericDAOImpl<Order> implements OrderDAO {
+    //------------------ Queries initialising ------------------------\\
+    @Override
+    String insertQuery() { return INSERT_ORDER; }
+    @Override
+    String selectQuery() { return SELECT_ORDER; }
+    @Override
+    String selectAllQuery() { return SELECT_ALL_ORDERS; }
+    @Override
+    String selectByFieldQuery() { return SELECT_ORDER_BY_FIELD; }
+    @Override
+    String updateQuery() { return UPDATE_ORDER; }
+    @Override
+    String deleteQuery() { return DELETE_ORDER; }
+
+    //------------------ DBStatementOperations initialising ------------------------\\
+    @Override
+    DBStatementOperations<Order> insertOperations() {
+        return (stmt, entity) -> {
+            stmt.setString(1, entity.getGuestsNumber());
+            stmt.setString(2, entity.getRoomsNumber());
+            stmt.setString(3, entity.getApartmentClass());
+            stmt.setDate(4, entity.getCheckOutDate());
+            stmt.setDate(5, entity.getCheckInDate());
+
+            stmt.setLong(6, entity.getAccount().getId());
+           //ResponseToOrder = NULL
+        };
+    }
+
+    @Override
+    DBStatementOperations<Order> updateOperations() {
+        return (stmt, entity) -> {
+            stmt.setString(1, entity.getGuestsNumber());
+            stmt.setString(2, entity.getRoomsNumber());
+            stmt.setString(3, entity.getApartmentClass());
+            stmt.setDate(4, entity.getCheckOutDate());
+            stmt.setDate(5, entity.getCheckInDate());
+
+           //Author Account unchanged
+          if(entity.getResponseToOrder() != null){
+            stmt.setLong(6, entity.getResponseToOrder().getId());}
+          else stmt.setNull(6, Types.INTEGER);
+            //WHERE
+            stmt.setLong(7, entity.getId());
+        };
+    }
+
+
+
+    //------------------ ExtractEntity initialising ------------------------\\
+    @Override
+    SetGeneratedValuesToEntity<Order> setGeneratedValuesToEntity() {
+        return (rs, entity) ->
+                entity.setId( (long) rs.getInt(1) );
+    }
+    @Override
+    ExtractEntity<Order> entityFromGet() {
+        return (rs) -> {
+
+            return Order.builder()
+                    .id(rs.getLong(ENTITY_ID))
+                    .guestsNumber(rs.getString(ORDER_GUESTS_NUMBER))
+                    .roomsNumber(rs.getString(ORDER_ROOMS_NUMBER))
+                    .apartmentClass(rs.getString(ORDER_APARTMENT_CLASS))
+                    .checkOutDate(rs.getDate(ORDER_CHECK_OUT_DATE))
+                    .checkInDate(rs.getDate(ORDER_CHECK_IN_DATE))
+                    //foreign keys
+                    .account(
+                            geAccountForeignKey(rs.getLong(ORDER_ACCOUNT_ID))
+                    )
+                    //may be null
+                    .responseToOrder(getResponseToOrderForeignKey(rs.getLong(ORDER_RESPONSE_TO_ORDER_ID)))
+                    .build();
+
+           /* Long  tempFK = rs.getLong(ORDER_RESPONSE_TO_ORDER_ID);
+            if(tempFK!=null) {
+                return Order.builder()
+                    .responseToOrder(getResponseToOrderForeignKey(tempFK))
+                    .build();
+            } else return Order.builder().build();*/
+        };
+    }
+
+    //foreign key
+    private Account geAccountForeignKey(Long id){
+        Connection connection= DBManager.getConnection();
+        AccountDAOImpl accountDAO = new AccountDAOImpl();
+
+        return accountDAO.get(connection, id);
+    }
+    //foreign key
+    private ResponseToOrder getResponseToOrderForeignKey(Long id){
+        Connection connection= DBManager.getConnection();
+        ResponseToOrderDAOImpl responseToOrderDAO = new ResponseToOrderDAOImpl();
+
+        return responseToOrderDAO.get(connection, id);
+    }
+
+}
---
+++ src/test/java/database/dao/OrderDAOTest.java
@@ -0,0 +1,100 @@
+package database.dao;
+
+import com.rosivanyshyn.db.dao.GenericDAO;
+import com.rosivanyshyn.db.dao.constant.Field;
+import com.rosivanyshyn.db.dao.entity.Account;
+import com.rosivanyshyn.db.dao.entity.Order;
+import com.rosivanyshyn.db.dao.implMySQL.OrderDAOImpl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+public class OrderDAOTest extends GenericDAOTest<Order>{
+    Order order;
+    @Override
+    protected GenericDAO<Order> setDAO() { return new OrderDAOImpl();}
+    @Override
+    protected Order setEntity() { return order; }
+    @Override
+    protected Long getEntityId(Order entity) { return entity.getId();}
+
+    //--------------Foreign keys temporary data----------------------\\
+    AccountDAOTest accountInitializer = new AccountDAOTest();
+    Account account;
+
+    @BeforeEach
+    void initialiseForeignKeys(){
+        //don`t clean BD after calling each method
+        accountInitializer.cleanDB = false;
+
+        accountInitializer.insertTestLogic(accountInitializer.insertEntity);
+
+        account = accountInitializer.entity;
+    }
+    @AfterEach
+    void destroyForeignKeys(){
+        accountInitializer.deleteTestLogic();
+    }
+    //--------------------------------------------------------------\\
+
+    protected BuildEntity<Order> insertEntity = ()-> order = Order.builder()
+            .id(0L)
+            .guestsNumber("4")
+            .roomsNumber("2")
+            .apartmentClass("B")
+            .checkInDate(
+                    Date.valueOf(LocalDate.of(2023, 01, 10))
+            )
+            .checkOutDate(
+                    Date.valueOf(LocalDate.of(2023, 01, 20))
+            )
+            .account(account)
+            .build();
+
+    protected BuildEntity<Order> updateEntity = ()-> Order.builder()
+            .id(order.getId())
+            .guestsNumber("6")
+            .roomsNumber("3")
+            .apartmentClass("C")
+            .checkInDate(order.getCheckInDate())
+            .checkOutDate(
+                    Date.valueOf(LocalDate.of(2023, 01, 27))
+            )
+            .build();
+
+    @Test
+    void insertTest(){
+        insertTestLogic(insertEntity);
+    }
+    @Test
+    void getTest(){
+        insertTestLogic(insertEntity);
+        getTestLogic();
+    }
+
+    @Test
+    void updateTest(){
+        insertTestLogic(insertEntity);
+        updateTestLogic(updateEntity);
+    }
+
+    @Test
+    void getAllTest(){
+        insertTestLogic(insertEntity);
+        getAllTestLogic();
+    }
+    @Test
+    void getByFieldTest(){
+        insertTestLogic(insertEntity);
+        getByFieldTestLogic(Field.ORDER_CHECK_IN_DATE, Date.valueOf(LocalDate.of(2023, 01, 10)));
+    }
+    @Test
+    void deleteTest(){
+        insertTestLogic(insertEntity);
+        deleteTestLogic();
+    }
+}
