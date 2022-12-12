d28c66a36ee4b42820bf4e626beed93d2ac402ae
Dragaar <rosivanyshyn@gmail.com>
2022-12-12 19:38:52 +0200

    Update DB structure and POM dependencies

--- src/main/java/com/rosivanyshyn/exeption/Message.java
+++ src/main/java/com/rosivanyshyn/exeption/Message.java
@@ -4,6 +4,8 @@ public class Message {
     public static final String INSERT_ERROR = "Cannot create row";
     public static final String SELECT_ERROR = "Cannot find row";
     public static final String SELECT_ALL_ERROR = "Cannot find all rows";
+
+    public static final String SELECT_FEW_ERROR = "Cannot find few rows";
     public static final String SELECT_BY_FIELD_ERROR = "Cannot find row by given field and value";
     public static final String UPDATE_ERROR = "Cannot update row by given object";
     public static final String DELETE_ERROR = "Cannot delete row by given id";
